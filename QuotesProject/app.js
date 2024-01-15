a41f5ab5665ea95ffa45159ac2d9f48a8c38eae1
444JJJ <155837608+444JJJ@users.noreply.github.com>
2024-01-15 09:07:37 +0000

    Add files via upload

---
+++ QuotesProject/app.js
@@ -0,0 +1,32 @@
+
+
+//Variables 
+let btn=document.querySelector('#new-quote');
+let quote=document.querySelector('.quote')
+let person=document.querySelector('.person');
+
+
+const quotes =[{
+    quote:`"The best way to find yourself is to lose
+     youself in the service of others."`,
+     person:`Mahatma Gandhi`
+},{
+    quote:`If you want to live a happy life, tie it to
+     a goal, not to people or things."`,
+    person:`Albert Einstein`
+},{
+    quote:`At his best, man is the noblest of all
+     animals; separated from law and justice he is the
+      worst."`,
+    person:`Aristotle`
+}
+];
+
+function newQuote(){
+    let random=Math.floor(Math.random()*quotes.length);
+    quote.innerText=quotes[random].quote;
+    person.innerText=quotes[random].person;
+}
+
+btn.addEventListener('click',newQuote);
+
